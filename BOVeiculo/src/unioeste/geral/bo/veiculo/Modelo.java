d4222f3772aa38d724e0fe4d484567705f1e3464
leoscalco <leoscalco@192.168.25.190>
2015-12-22 13:35:08 -0200

    Classes de negócio Veiculo

---
+++ BOVeiculo/src/unioeste/geral/bo/veiculo/Modelo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unioeste.geral.bo.veiculo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author leoscalco
+ */
+public class Modelo implements Serializable{
+    int codigo;
+    String nome;
+    Marca marca;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+    
+    
+}
---
+++ BOVeiculo/src/unioeste/geral/bo/veiculo/Veiculo.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unioeste.geral.bo.veiculo;
+
+import java.io.Serializable;
+import java.util.Date;
+import unioeste.manutencao.bo.cliente.Cliente;
+
+/**
+ *
+ * @author leoscalco
+ */
+public class Veiculo implements Serializable{
+    int codigo;
+    String placa;
+    String chassi;
+    Date ano;
+    String cor;
+    Modelo modelo;
+    Cliente cliente;
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+
+    public Date getAno() {
+        return ano;
+    }
+
+    public void setAno(Date ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+    
+    
+}
