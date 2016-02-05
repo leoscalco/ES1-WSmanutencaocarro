6e1de5fc1364433653f5dab0254afddc8c5a8e09
leoscalco <leoscalco@179.106.204.8>
2016-02-05 11:13:29 -0200

    Infra com pool no JBOSS

--- InfraManutencaoCarro/src/unioeste/manutencao/infra/configuracao/ConexaoMySQL.java
+++ InfraManutencaoCarro/src/unioeste/manutencao/infra/configuracao/ConexaoMySQL.java
@@ -10,7 +10,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 import java.sql.SQLException;
-
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 /**
  *
  * @author leoscalco
@@ -24,21 +26,28 @@ public class ConexaoMySQL {
     }
 
     //Método de Conexão// 
-    public static java.sql.Connection getConexaoMySQL() {
+    public static java.sql.Connection getConexaoMySQL() throws Exception {
         Connection connection = null;
         //atributo do tipo Connection 
-        try {
             // Carregando o JDBC Driver padrão 
-            String driverName = "com.mysql.jdbc.Driver";
-            Class.forName(driverName);
+//            String driverName = "com.mysql.jdbc.Driver";
+//            Class.forName(driverName);
+//
+//            // Configurando a nossa conexão com um banco de dados// 
+//            String serverName = "localhost"; //caminho do servidor do BD 
+//            String mydatabase = "SistemaManutencao"; //nome do seu banco de dados 
+//            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+//            String username = "root"; //nome de um usuário de seu BD 
+//            String password = "root"; //sua senha de acesso 
+//            connection = DriverManager.getConnection(url, username, password);
+            
+//            InitialContext context = new InitialContext();
+//            DataSource ds = (DataSource)context.lookup("MysqlDS");
 
-            // Configurando a nossa conexão com um banco de dados// 
-            String serverName = "localhost"; //caminho do servidor do BD 
-            String mydatabase = "SistemaManutencao"; //nome do seu banco de dados 
-            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
-            String username = "root"; //nome de um usuário de seu BD 
-            String password = "root"; //sua senha de acesso 
-            connection = DriverManager.getConnection(url, username, password);
+            Context context = new InitialContext();
+            DataSource ds = (DataSource)context.lookup("java:jboss/datasources/MySqlDS");
+
+            connection = ds.getConnection();
 
             //Testa sua conexão// 
             if (connection != null) {
@@ -47,15 +56,7 @@ public class ConexaoMySQL {
                 status = ("STATUS--->Não foi possivel realizar conexão");
             }
             return connection;
-        } catch (ClassNotFoundException e) {
-            //Driver não encontrado 
-            System.out.println("O driver expecificado nao foi encontrado.");
-            return null;
-        } catch (SQLException e) {
-            //Não conseguindo se conectar ao banco 
-            System.out.println("Nao foi possivel conectar ao Banco de Dados.");
-            return null;
-        }
+      
     }
 
 //Método que retorna o status da sua conexão// 
@@ -64,7 +65,7 @@ public class ConexaoMySQL {
     }
 
 //Método que fecha sua conexão// 
-    public static boolean FecharConexao() {
+    public static boolean FecharConexao() throws Exception {
         try {
             ConexaoMySQL.getConexaoMySQL().close();
             return true;
@@ -74,7 +75,7 @@ public class ConexaoMySQL {
     }
 
 //Método que reinicia sua conexão// 
-    public static java.sql.Connection ReiniciarConexao() {
+    public static java.sql.Connection ReiniciarConexao() throws Exception {
         FecharConexao();
         return ConexaoMySQL.getConexaoMySQL();
     }
